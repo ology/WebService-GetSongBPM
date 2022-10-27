26ed4c716f565b2274a23dbce04aba2787b5dbd9
Gene Boggs <gene@ology.net>
2022-10-26 18:21:36 -0700

    Avoid implicit imports

--- lib/WebService/GetSongBPM.pm
+++ lib/WebService/GetSongBPM.pm
@@ -4,16 +4,14 @@ package WebService::GetSongBPM;
 
 our $VERSION = '0.0400';
 
-use Moo;
 use strictures 2;
-use namespace::clean;
-
-use Carp;
-use Mojo::UserAgent;
-use Mojo::JSON::MaybeXS;
-use Mojo::JSON qw( decode_json );
-use Mojo::URL;
+use Carp qw(croak);
+use Mojo::UserAgent ();
+use Mojo::JSON qw(decode_json);
+use Mojo::URL ();
+use Moo;
 use Try::Tiny;
+use namespace::clean;
 
 =head1 SYNOPSIS
 
